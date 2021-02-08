d71f728761441d00b13aabafb96547f0483a38ba
RoninNada <zerodev@gmx.com>
2021-02-09 09:04:05 +1000

    Removed roomservice

--- envsetup.sh
+++ envsetup.sh
@@ -623,20 +623,6 @@ function lunch()
     fi
 
     check_product $product
-    if [ $? -ne 0 ]
-    then
-        # if we can't find a product, try to grab it off the AOSiP-Devices GitHub
-        T=$(gettop)
-        cd $T > /dev/null
-        vendor/aosip/build/tools/roomservice.py $product
-        cd - > /dev/null
-        check_product $product
-    else
-        T=$(gettop)
-        cd $T > /dev/null
-        vendor/aosip/build/tools/roomservice.py $product true
-        cd - > /dev/null
-    fi
 
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
