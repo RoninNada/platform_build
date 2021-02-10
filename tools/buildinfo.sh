e10a37fba2b404f29525089ea2cad43740de8087
RoninNada <zerodev@gmx.com>
2021-02-11 08:21:00 +1000

    Revert:build: add maintainers to build.prop 8b14e113ae6d7fdd29c871e354c8f43cced8f2d5

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -60,9 +60,6 @@ if [ "$TARGET_UNIFIED_DEVICE" == "" ] ; then
     echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
   fi
 fi
-if [ -n "$DEVICE_MAINTAINERS" ] ; then
-  echo "ro.aosip.maintainer=$DEVICE_MAINTAINERS"
-fi
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 
 echo "ro.aosip.device=$AOSIP_DEVICE"
